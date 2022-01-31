ba04c8fc2e231e9fbe7c559ab3105504db47fbde
Bluewaves54 <88750406+Bluewaves54@users.noreply.github.com>
2022-01-31 15:49:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 file = open('allWords.txt')
-
-allWords = list()
-for word in file:
-  allWords.append(word.strip())
-
-filteredWords = list()
-
-command = None
-commands = list()
-while command != 'finish':
+allWords = [word.strip() for word in file]
+command, commands = None, list()
+while True:
   command = input('>')
+  if command == 'finish':
+    break
   commands.append(command)
-
 finalcmd = str()
-for command in commands:
-  finalcmd += command
-  try:
-    if command == commands[-2]:
-      break
-    else:
-      finalcmd += ' and '
-  except IndexError:
-    finalcmd = 'True'
-
-for word in allWords:
-  if eval(finalcmd):
-    filteredWords.append(word)
-  else:
-    continue
-
-for word in filteredWords:
-  print(word)
+if len(commands) > 0:
+  for command in commands:
+    finalcmd += (command + ' and ')
+else:
+  finalcmd = 'True'
+filteredWords = [word for word in allWords if eval(finalcmd)]
+print(word for word in filteredWords)
